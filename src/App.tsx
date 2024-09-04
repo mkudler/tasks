433ebb962cf566800412b6aab53ce8b980e29fa8
Matthew Kudler <mkudler@udel.edu>
2024-09-04 17:33:19 -0400

    This is being accomplished

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,56 @@ function App(): React.JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
+                <h1>Welcome to My React App</h1>
                 Hello World Matthew Kudler UD CISC275 with React Hooks and
                 TypeScript
             </header>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
+            <img
+                src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pexels.com%2Fsearch%2Fdog%2F&psig=AOvVaw0cvn2L-j9F8UhXdcWosSzl&ust=1725571200739000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCICbuOabqogDFQAAAAAdAAAAABAK"
+                alt="Random dog image"
+            />
+            <ul>
+                <li>I</li>
+                <li>Did</li>
+                <li>It</li>
+            </ul>
+            <button
+                className="btn btn-primary"
+                onClick={() => {
+                    console.log("Hello World!");
+                }}
+            >
+                Log Hello World
+            </button>
+            <div
+                style={{
+                    display: "flex",
+                    justifyContent: "space-around",
+                    marginTop: "20px",
+                }}
+            >
+                <div
+                    style={{
+                        width: "100px",
+                        height: "100px",
+                        backgroundColor: "red",
+                    }}
+                ></div>
+                <div
+                    style={{
+                        width: "100px",
+                        height: "100px",
+                        backgroundColor: "red",
+                    }}
+                ></div>
+                <div
+                    style={{
+                        width: "100px",
+                        height: "100px",
+                        backgroundColor: "red",
+                    }}
+                ></div>
+            </div>
         </div>
     );
 }
