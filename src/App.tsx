328bfb6a7b4f014edb7cbc21f7e3b8da2f670609
Matthew Kudler <mkudler@udel.edu>
2024-09-05 15:11:13 -0400

    Being committed

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,72 @@
 import React from "react";
 import "./App.css";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 function App(): React.JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">
-                <h1>Welcome to My React App</h1>
-                Hello World Matthew Kudler UD CISC275 with React Hooks and
-                TypeScript
-            </header>
-            <img
-                src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pexels.com%2Fsearch%2Fdog%2F&psig=AOvVaw0cvn2L-j9F8UhXdcWosSzl&ust=1725571200739000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCICbuOabqogDFQAAAAAdAAAAABAK"
-                alt="Random dog image"
-            />
-            <ul>
-                <li>I</li>
-                <li>Did</li>
-                <li>It</li>
-            </ul>
-            <button
-                className="btn btn-primary"
-                onClick={() => {
-                    console.log("Hello World!");
-                }}
-            >
-                Log Hello World
-            </button>
-            <div
-                style={{
-                    display: "flex",
-                    justifyContent: "space-around",
-                    marginTop: "20px",
-                }}
-            >
-                <div
-                    style={{
-                        width: "100px",
-                        height: "100px",
-                        backgroundColor: "red",
-                    }}
-                ></div>
-                <div
-                    style={{
-                        width: "100px",
-                        height: "100px",
-                        backgroundColor: "red",
-                    }}
-                ></div>
-                <div
-                    style={{
-                        width: "100px",
-                        height: "100px",
-                        backgroundColor: "red",
-                    }}
-                ></div>
+            <h1>Hello World</h1>
+            <div>
+                <Container>
+                    <Row>
+                        <div
+                            style={{
+                                width: "100px",
+                                height: "100px",
+                                backgroundColor: "red",
+                            }}
+                        >
+                            <Col>
+                                <header className="App-header">
+                                    Hello World Matthew Kudler UD CISC275 with
+                                    React Hooks and TypeScript
+                                </header>
+                                <div
+                                    style={{
+                                        backgroundColor: "2px solid red",
+                                        padding: "4px",
+                                    }}
+                                ></div>
+                                <ul>
+                                    <li>I</li>
+                                    <li>Did</li>
+                                    <li>It</li>
+                                </ul>
+                            </Col>
+                        </div>
+                        <div
+                            style={{
+                                width: "100px",
+                                height: "100px",
+                                backgroundColor: "red",
+                            }}
+                        >
+                            <Col>
+                                <div>
+                                    <div>
+                                        <Button
+                                            onClick={() => {
+                                                console.log("Hello World!");
+                                            }}
+                                        >
+                                            Log Hello World
+                                        </Button>
+                                    </div>
+                                    <img
+                                        src="C:/Users/Matthew Kudler/Pictures/20190305_142745.jpg"
+                                        alt="A picture of my dogs Summer and Munchkin"
+                                    />
+                                </div>
+                            </Col>
+                        </div>
+                    </Row>
+                </Container>
+            </div>
+            <div>
+                <p>
+                    Edit <code>src/App.tsx</code> and save. This page will
+                    automatically reload.
+                </p>
             </div>
         </div>
     );
